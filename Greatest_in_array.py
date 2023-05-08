b9a84ff167af04bc2fa196912fc854fada0b5d4e
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-05-09 00:20:10 +0530

    Create Greatest_in_array.py

---
+++ Greatest_in_array.py
@@ -0,0 +1,36 @@
+def largest(arr, n):
+
+ 
+
+
+    max = arr[0]
+
+
+
+    
+
+
+
+for i in range(1, n):
+
+        if arr[i] > max:
+
+            max = arr[i]
+
+    return max
+
+ 
+
+ 
+
+# Driver Code
+
+arr = [10, 324, 45, 90, 9808]
+
+n = len(arr)
+
+Ans = largest(arr, n)
+
+print("Largest in given array ", Ans)
+
+ 
