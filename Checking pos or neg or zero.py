c51f50542f1678da2ad659b1bf709aaccd33282c
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-06-12 22:29:57 +0530

    Update Checking pos or neg or zero.py

--- Checking pos or neg or zero.py
+++ Checking pos or neg or zero.py
@@ -1,11 +1,11 @@
 number = float(input("Enter a number: "))
-
+#check for positive
 if num > 0:
    print("Positive number")
 #checking if 0
 elif number == 0:
    print("Zero")
    
-
+#if both fails then consider it has negative 
 else:
    print("Negative number")
