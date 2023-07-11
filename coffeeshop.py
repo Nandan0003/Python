52a987efdccf6d1039d2ac4c2bccb9058b217f75
Nandan <nandankumar3m@gmail.com>
2023-07-11 17:34:11 +0530

    coffee shop designed

---
+++ coffeeshop.py
@@ -0,0 +1,37 @@
+print("welcome to the shop")
+
+#asking name
+
+name = input("enter ur name")
+
+print("what would you like to have "+" " + name )
+
+#Taking order
+order = input("normal,black,strong,cold" )
+
+if order == "normal":
+    more = input("do u want more sugar")
+    if more == "yes":
+        price = 25
+    else:
+        price = 20
+           
+elif order == "black":
+    price = 12
+elif order == "strong":
+    price =13
+elif order == "cold":
+    price = 15
+else :
+    print("sorry we dont have that")
+    exit()
+#asking for quantity
+quantity = int(input("how many coffee do u want"))
+
+
+totalprice = quantity * price
+
+print("ur total bills is   " + str(totalprice))
+
+
+
