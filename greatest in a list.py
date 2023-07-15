4253213db32e687e5950d3bfdf6b69c76ab1c9fe
Nandan <nandankumar3m@gmail.com>
2023-07-15 21:30:04 +0530

    greatest in list

---
+++ greatest in a list.py
@@ -0,0 +1,7 @@
+list = [1,4,5,3]
+max = list[0]
+
+for i in list:
+    if i > max:
+        max = i
+print(max)
--- kg2pnd.py
+++ kg2pnd.py
@@ -1,8 +1,6 @@
-#enter weight in pounds
 weight_in_pound =  int(input(" enter ur weight\n"))
-#calculating weight
+
 weight_in_kg= weight_in_pound *0.45
-#representing weight in kg
+
 print(weight_in_kg)
-#representing weight's datatype
 print(type(weight_in_kg))
