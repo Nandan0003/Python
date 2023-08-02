2d4f9c5bfcdadaf989b4c7ee119f549765714861
Nandan <nandankumar3m@gmail.com>
2023-08-02 21:14:57 +0530

    binary search

---
+++ binary_search.py
@@ -0,0 +1,41 @@
+#initializing pos value to 0
+pos = 0
+
+#defining a function which returns true if find else return false
+def b(ls,n):
+    l = 0
+    u = len(ls) - 1
+    print(f'element to search is {n}')
+    while l <= u:
+    
+        mid = (l+u) // 2
+        
+        if ls[mid] == n:
+            globals()['pos'] = mid
+            return True
+        else:
+            if ls[mid] < n:
+                l = mid 
+            else:
+                u = mid 
+                
+    return False    
+ 
+#list 
+ls  = [5,1,6,7,8,9,10]
+
+#sorting a list
+ls.sort()
+
+#printing of sorted list
+print(ls)
+
+# value which we require
+n = 9
+
+
+#calling the function and checking if it is true or not
+if b(ls,n) == True:
+    print("found at",pos )
+else:
+    print("not found")        
