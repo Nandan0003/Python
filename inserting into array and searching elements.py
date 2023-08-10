cc00899c2b3bf5869300109550b3762e50cc635f
Nandan kumar M <nandankumar3m@gmail.com>
2023-08-10 21:47:27 +0530

    Update inserting into array and searching elements.py

--- inserting into array and searching elements.py
+++ inserting into array and searching elements.py
@@ -1,17 +1,25 @@
+#importing array module
 import array as ar
 
+#initiazing empty array
 arr = ar.array('i',[])
 
+#asking size of array from user
 n = int(input("enter size of array \n"))
 
+#getting elements from user
 for i in range(n):
     ele = int(input("enter the values \n"))
     arr.append(ele)
     
+#printing array    
 print(arr)
 
+#value to search
 value = int(input("enter a value to search"))
 
+
+#searching element
 pos = 0
 
 for j in arr:
@@ -20,3 +28,11 @@ for j in arr:
     pos += 1
 else:
     print("not found")
+
+
+
+
+
+
+
+
