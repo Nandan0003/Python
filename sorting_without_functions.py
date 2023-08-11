a52d9debbbfee79291b20c79f98418851168fd21
Nandan <nandankumar3m@gmail.com>
2023-08-11 22:00:13 +0530

    .....

--- sorting_without_functions.py
+++ sorting_without_functions.py
@@ -1,14 +1,14 @@
+#initializing list
 list1 = [5,2,4,1,6,4,8]
 
+#checking cond if 1st ele to nth element and swap
 for i in range(0,len(list1)):
     for j in range(i+1,len(list1)):
         if list1[i] >= list1[j]:
             list1[i],list1[j] = list1[j],list1[i]
+
+#print final list
             
 print(list1)
 
 
-list2 = [ 5,6,7,5,6,49,9]
-
-lits2 = list2[ , ,-1]
-print(list2)
