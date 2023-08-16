12988e62de0e4fb7b8beb85305fdd6395ca343ae
Nandan <nandankumar3m@gmail.com>
2023-08-16 22:50:34 +0530

    simple

---
+++ strcmp.py
@@ -0,0 +1,20 @@
+def str():
+   
+    c = a.lower()
+    d = b.lower()
+
+
+    if a == b:
+        print("matching")
+    elif  c == d :
+        print("matching with different case")
+        
+    else:
+        print("not matching")
+        
+        
+a = input("enter name-> \n")
+
+b = input("enter another name-> \n")
+
+str()
