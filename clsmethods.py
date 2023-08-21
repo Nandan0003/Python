aaf7281be04767b0f2c22dc574bc87b16ae2f34a
Nandan <nandankumar3m@gmail.com>
2023-08-21 20:21:21 +0530

    ..

--- clsmethods.py
+++ clsmethods.py
@@ -1,6 +1,6 @@
 class movies:
     
-
+   
     
     def __init__(self,hero,heroine,year,movie):
         self.hero = hero
@@ -12,9 +12,15 @@ class movies:
     def title(self):
         print(f'{self.hero} is with {self.heroine} and in year {self.year} for a big movie called  {self.movie} ')
         
+    
+        
+        
 y = movies("yash","srinidhi",2019,"kgf")
 y.title()
 
 
 s = movies("sudeep","neetha ashok",2022,"vikrath rona")
 s.title()
+
+sh = movies("shivanna","adithi",2023,"vedha")
+sh.title()
