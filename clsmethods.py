067781123f03854361a227a6ecf8b286f1042493
Nandan <nandankumar3m@gmail.com>
2023-08-21 20:15:36 +0530

    new method

---
+++ clsmethods.py
@@ -0,0 +1,20 @@
+class movies:
+    
+
+    
+    def __init__(self,hero,heroine,year,movie):
+        self.hero = hero
+        self.heroine = heroine
+        self.year = year
+        self.movie = movie
+        
+        
+    def title(self):
+        print(f'{self.hero} is with {self.heroine} and in year {self.year} for a big movie called  {self.movie} ')
+        
+y = movies("yash","srinidhi",2019,"kgf")
+y.title()
+
+
+s = movies("sudeep","neetha ashok",2022,"vikrath rona")
+s.title()
