dfe2b013e4ba3c9ccaac58f282d3257180813bee
Nandan <nandankumar3m@gmail.com>
2023-08-22 22:10:57 +0530

    update

--- nested classes.py
+++ nested classes.py
@@ -3,17 +3,19 @@ class Bike:
         self.brand = brand 
         self.ccubic = ccubic
         self.cylinder = cylinder
-        self.s = self.show()
+        
     def show(self):
         print(f'{self.brand} has a {self.ccubic} capacity bike with {self.cylinder} cylinders')
         
         
     class Model:
-        def __init__(self):
-            self.bname = "BMW"
+        def __init__(self,bname):
+            self.bname = bname
             
-        def show(self):
-            return self.bname
+        def brand(self):
+            print(f'{self.bname}')
 a = Bike("BMW",1000,4)
 a.show()
-a.Model.bname
+
+b = Bike.Model("S1000RR")
+b.brand()
