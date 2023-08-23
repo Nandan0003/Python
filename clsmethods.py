ba83de27287710b297577c7da2697a7e71dadfb0
Nandan <nandankumar3m@gmail.com>
2023-08-23 20:01:11 +0530

    update

--- clsmethods.py
+++ clsmethods.py
@@ -1,6 +1,6 @@
 class movies:
     
-   
+    industry = "SANDALWOOD"
     
     def __init__(self,hero,heroine,year,movie):
         self.hero = hero
@@ -13,14 +13,27 @@ class movies:
         print(f'{self.hero} is with {self.heroine} and in year {self.year} for a big movie called  {self.movie} ')
         
     
+    @classmethod
+    def indus(cls):
+        print(cls.industry)  
+
+    @staticmethod
+    def info():
+        print("\nall are HIT movies in KANNADA andEVERYWHERE\n")
         
         
+        
+        
+movies.indus()       
 y = movies("yash","srinidhi",2019,"kgf")
 y.title()
 
-
+movies.indus()  
 s = movies("sudeep","neetha ashok",2022,"vikrath rona")
 s.title()
 
+movies.indus()  
 sh = movies("shivanna","adithi",2023,"vedha")
 sh.title()
+
+movies.info()
