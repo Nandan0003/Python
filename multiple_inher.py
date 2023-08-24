97fac7cd67207e5c5e1880c05e092148d2028ae0
Nandan <nandankumar3m@gmail.com>
2023-08-24 20:08:12 +0530

    new update

--- multiple_inher.py
+++ multiple_inher.py
@@ -21,10 +21,24 @@ class C(A,B):
     def __init__(self,a,b):
         self.a = a
         self.b = b
-        super().__init__(1,2)
+        super().__init__(a,b)
         print("c class")
     def mul(self):
         print(int(self.a * self.b))
+        
+        
+class D(C):
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        print("class d")
+
+    def div(self):
+        try:
+            dd = (self.a / self.b)
+            print(dd)
+        except ZeroDivisionError:
+            print(f'{self.b} cant be zero')
 
 
 a1 = A(4,5)
@@ -32,3 +46,6 @@ a1.add()
 
 a2 = C(56,65)
 a2.mul()
+
+a3 = D(4,5)
+a3.sub()
