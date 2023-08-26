c6bf7d0a09a29ee4743b84cb7e05d381004051a9
Nandan <nandankumar3m@gmail.com>
2023-08-26 20:02:33 +0530

    neww methods

---
+++ counting space.py
@@ -0,0 +1,8 @@
+def spaces(str):
+    count = 0
+    for i in str:
+        if i == " ":
+            count += 1
+    print(count)     
+str = input("enter a string ")
+spaces(str)
---
+++ ducktyping.py
@@ -0,0 +1,21 @@
+class Bike:
+    def start(self):
+        print("Bike is ready to go")
+
+class Car:
+    def start(self):
+        print("Car is ready to go")
+        
+        
+class BMW:
+    def new(self,bb):
+        bb.start()
+        
+bb = Bike()
+
+b = BMW()
+b.new(bb)
+
+cc = Car()
+c =BMW()
+c.new(cc)
