1f994b4b2f23fd5e89cfc91ffb3a6921a64d330d
Nandan <nandankumar3m@gmail.com>
2023-08-27 20:05:07 +0530

    update

--- operator_overloading.py
+++ operator_overloading.py
@@ -11,10 +11,18 @@ class Emp:
         bon = self.bon + other.bon 
         combined = Emp(sal,bon)
         return combined
-        
+
+    def __sub__(self,other):
+        sal = self.sal - other.sal 
+        bon = self.bon - other.bon 
+        divided  = Emp(sal,bon)
+        return divided
         
 husband = Emp(10000,1000)
 wife = Emp(9000,900)
 
 combined = husband + wife 
 print(combined.sal)
+
+divided = husband - wife
+print(divided.bon)
