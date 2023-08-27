accf09b66e7e563f3750ebcf02cafc3fbaed383b
Nandan <nandankumar3m@gmail.com>
2023-08-27 19:54:35 +0530

    new

---
+++ operator_overloading.py
@@ -0,0 +1,20 @@
+class Emp:
+    def __init__(self,sal,bon):
+        self.sal = sal
+        self.bon = bon
+        
+        
+        
+        
+    def __add__(self,other):
+        sal = self.sal + other.sal 
+        bon = self.bon + other.bon 
+        combined = Emp(sal,bon)
+        return combined
+        
+        
+husband = Emp(10000,1000)
+wife = Emp(9000,900)
+
+combined = husband + wife 
+print(combined.sal)
