dee8488f4cac8fa8c4bde75a1322ae7c80965925
Nandan kumar M <nandankumar3m@gmail.com>
2023-09-05 20:38:36 +0530

    Update countingchar.py

--- countingchar.py
+++ countingchar.py
@@ -1,10 +1,15 @@
 def counting(str):
     count = 0
+    space = 0
     for i in str:
         count += 1
+
+        if i == " ":
+            space += 1
+            
         
     print("the no of characters in string is",count)
-
-str = input("enter a string \n")
+    print(f'the string has {space} no of spaces'}
+    str = input("enter a string \n")
 
 counting(str)
