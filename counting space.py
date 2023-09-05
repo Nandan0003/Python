99c780466d84b715700b2e105022fad881fefab3
Nandan <nandankumar3m@gmail.com>
2023-09-05 20:06:19 +0530

    new

--- counting space.py
+++ counting space.py
@@ -6,3 +6,21 @@ def spaces(str):
     print(count)     
 str = input("enter a string ")
 spaces(str)
+
+
+
+a  = "nanfdan"
+n = a[0]
+
+for i in a:
+    if i == n:
+        print("repeats",i)
+
+def lenth(str):
+    count = 0
+    for i in str:
+        count += 1
+    print(count)
+
+str = input("enter a string")
+lenth(str)
