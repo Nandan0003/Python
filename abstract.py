be7a16ab88538ca6ae332a797eaac5eb2af39004
Nandan <nandankumar3m@gmail.com>
2023-09-06 19:45:07 +0530

    abstact method:

---
+++ abstract.py
@@ -0,0 +1,19 @@
+from abc import ABC,abstractmethod
+
+class Bike(ABC):
+    @abstractmethod
+    def top(self):
+        pass
+    
+    
+class BMW(Bike):
+    def top(self):
+        print("top speed of 300kmph")
+        
+class Ducati(Bike):
+    def top(self):
+        print("top speed of 400kmph")
+        
+        
+b1 = BMW()
+b1.top()
