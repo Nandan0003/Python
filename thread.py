37c138d11d735ee998debd02f1403838dc3dba51
Nandan <nandankumar3m@gmail.com>
2023-09-13 20:09:31 +0530

    multi

---
+++ thread.py
@@ -0,0 +1,23 @@
+from  threading import Thread
+from time import sleep
+
+
+
+def Car():
+    for i in range(5):
+        print("my new car")
+        sleep(2)
+def Bike():
+    for i in range(5):
+        print("my new bike ")
+        sleep(2)
+        
+c =Thread( target=Car,args= ())       
+c.start()
+
+
+b =Thread( target=Bike,args= ())       
+b.start()
+c.join()
+b.join()
+print("done")
