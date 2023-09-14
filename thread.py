fed8e902723db3bacf062a69aa94202aac5cf1b3
Nandan <nandankumar3m@gmail.com>
2023-09-14 19:43:36 +0530

    ..

--- thread.py
+++ thread.py
@@ -11,7 +11,14 @@ def Bike():
     for i in range(5):
         print("my new bike ")
         sleep(2)
-        
+
+class Super(Thread):
+    def Bike(self):
+        for i in range(5):
+            print("my new bike ")
+            sleep(2)
+    
+
 c =Thread( target=Car,args= ())       
 c.start()
 
@@ -20,4 +27,7 @@ b =Thread( target=Bike,args= ())
 b.start()
 c.join()
 b.join()
+
+s = Super()
+s.start()
 print("done")
