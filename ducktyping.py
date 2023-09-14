67850bc157e3a8c776126c4320303c93c8a2847e
Nandan <nandankumar3m@gmail.com>
2023-09-14 21:02:29 +0530

    ..

--- ducktyping.py
+++ ducktyping.py
@@ -28,20 +28,3 @@ d = BMW()
 d.new(dd)
 
 
-class sub:
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
-        
-    def add(self):
-        print(self.a + self.b)
-    def mul(self):
-        print(self.a * self.b)
-
-class Main:
-    
-    a = sub(20,0)
-    a.add()
-    a.mul()
-
-aa = Main()
