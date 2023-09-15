2bfffd11b7918b507c9f49b268bead5cd23c03ba
Nandan <nandankumar3m@gmail.com>
2023-09-15 21:05:55 +0530

    ..

---
+++ anotherduck.py
@@ -0,0 +1,30 @@
+#class pen
+class Pen:
+    def write(self):
+        print("pen will write")
+#class pencil        
+class Pencil:
+    def write(self):
+        print("pencil will write")
+#class book       
+class book:
+    def write(self):
+        print("bokk is both readble and writeable")
+#class Any which is ducktyped can calledby any above classes the obejcts   
+class Any:
+    def any(self,objj):
+        objj.write()
+        
+        
+#object for pen       
+ob1 = Pen()
+
+
+#object for ducktyped class
+ob2 = Any()
+#calling function and passing pen object 
+ob2.any(ob1)
+
+
+
+#any class and objects can be passed to any so it calls directly
