0045fa5b1a4925c345da1b4fe36230ecd4bc0989
Nandan <nandankumar3m@gmail.com>
2023-09-21 20:20:55 +0530

    ..

--- replace.py
+++ replace.py
@@ -3,3 +3,7 @@ a = "am nandan kumar from bengaluru"
 b = a.replace("am","new")
 
 print(b)
+
+
+a = 7 < 6 
+print(a)
---
+++ selectonsort.py
@@ -0,0 +1,34 @@
+def sortt(lisss):
+    #n is length of listt passed
+    
+    n = len(lisss)
+    
+    #for loop from 0 to end of list
+    for i in range(0,n):
+        
+        
+        #assuming first elent is minimum
+        min = i 
+        
+        
+        #nested loop for checking condition min is less than next elements
+        for j in range(i,n):
+            
+            
+            #checking j value is less than value
+            if lisss[j] < lisss[min]:
+                
+                
+                #if j < min then reinitialize j as min
+                min = j
+                
+                
+        #swap i and min       
+        lisss[i],lisss[min] = lisss[min],lisss[i]
+        
+        
+        print(lisss)   
+
+
+lisss = [5,6,7,2,6,9,5]
+sortt(lisss)
