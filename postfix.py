4d0983f8b40273b6aaaca2a50ab02008581534ec
Nandan <nandankumar3m@gmail.com>
2023-09-27 22:02:44 +0530

    update

--- postfix.py
+++ postfix.py
@@ -1,11 +1,20 @@
+#declraing funxtion 
 def postfix(exp):
+    #to store values calliong a list
     digitt = []
+    #checking each and every element in expression
     for i in exp:
+        #if the element iss digit then insert into lisit
         if i.isdigit():
             digitt.append(int(i))
+        #else calculating exp
         else:
+            #the first element coming out will be 2nd oprand as it is inserrted 2nd
             op2 = digitt.pop()
+            #the seco9nd elemt coming out will be 1st operand as it is inserted 1st
             op1 = digitt.pop()
+            
+            #operators and their function
             if i == "+":
                 res = op1 + op2
             elif i == "-":
@@ -15,6 +24,7 @@ def postfix(exp):
             elif i == "/":
                 res = op1 / op2
             digitt.append(res)
+    #return the elemnt of list at end of execution
     return digitt[0]
 
 exp = "34+5-"
