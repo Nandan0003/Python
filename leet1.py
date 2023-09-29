d87b5578badb51a4de3b31a8afea519c520eb805
Nandan <nandankumar3m@gmail.com>
2023-09-29 19:28:32 +0530

    new

---
+++ leet1.py
@@ -0,0 +1,24 @@
+#progrann to combine letters of string 
+word1 = "abc" 
+
+word2 = "pqr"
+
+result = ""
+
+i = 0 
+while i < 3:
+    res = word1[i] + word2[i]
+    result += res
+    
+    i += 1
+    
+print(result)
+
+
+num1 = [0,1,2,2]
+num2 = [0,3,5,6]
+num3 = []
+z = []
+
+n = len(num1)
+
