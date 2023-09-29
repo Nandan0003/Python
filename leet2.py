760a74e912ec5ce1a4397a2163d9730350285dea
Nandan <nandankumar3m@gmail.com>
2023-09-29 20:03:56 +0530

    2

---
+++ leet2.py
@@ -0,0 +1,32 @@
+#problem to combine to or more list which does not accepts 0
+num1 = [1,2,3,4]
+num2 = [5,6,7]
+num3 = []
+
+n = len(num1)
+i = 0
+while i < n:
+    if num1[i] != 0:
+        num3.append(num1[i])
+    i += 1
+        
+n = len(num2)
+i = 0
+while i < n:
+    if num2[i] != 0:
+        num3.append(num2[i])
+    i += 1
+
+
+print(num3)
+
+n = len(num1)
+i = 0
+while i < n:
+    if num1[i] != 0:
+        print(num1)
+        num1.insert(i,num1[i])
+    i += 1
+        
+    
+    
