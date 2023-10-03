ce68df3e6ea021ffaa7b555f58ad9e11ed904a53
Nandan <nandankumar3m@gmail.com>
2023-10-03 22:44:42 +0530

    new

---
+++ servers.py
@@ -0,0 +1,15 @@
+class Server1:
+    print("server is being used by users to send requests")
+    
+class Server2:
+    print("Server is used to send information to users")
+    
+class Server3:
+    print("used as storage of all information")
+    
+class main:
+    a = Server1()
+    b = Server2()
+    c = Server3()
+    
+m = main()
