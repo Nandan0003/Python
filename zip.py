d42cec01106ecb4dfee20bec4e07568001eb4082
Nandan <nandankumar3m@gmail.com>
2023-10-10 20:04:24 +0530

    method two

--- zip.py
+++ zip.py
@@ -1,13 +1,21 @@
 #zip function is used to combine two or more list or tuple like structure to provide output as each element in preferred type
 
 
-
+#A list which has some characters
 a = ["N","A","N","D","A","N"]
-
+#B list which has some characters
 b = ["B","N","A","N","A","V"]
 
 z = list(zip(a,b))
 print(z)
 
-for (a,b) in z:
+a = ["a","ab","t", "co", "i", "pyt"]
+b = ["m","le","o","de","n","hon"]
+
+coder = list(zip(a,b)) 
+
+print("\n\n\n")
+
+print("read by neglecting spaces")
+for (a,b) in coder:
     print(a,b)
