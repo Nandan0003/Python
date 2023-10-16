0707a21e7185bebbd3d94fdeeb40db3eacde8867
Nandan <nandankumar3m@gmail.com>
2023-10-16 22:09:42 +0530

    new methods of automation

--- auto.py
+++ auto.py
@@ -1,13 +1,24 @@
 cond = False 
 
 def light():
-    clap = int(input("from censor returning 1 if clap is heard else 0 not heard \n"))
-    if clap == 1:
-        print("turning on lamp ")
-        globals()['cond'] = True
-        
-    else:
-        print("not heard and not turning lamp")
+    clapcount = int(input("from censor returning count of claps \n"))
+    if clapcount == 0:
+        print("not heard and not turning on anything")
         globals()['cond'] =  False
 
+    elif clapcount == 1:
+        print("turning on lamp ")
+        globals()['cond'] = True
+    
+    elif clapcount  == 2:
+        print("turning on fan")  
+        globals()['cond'] = True
+    elif clapcount  == 3:
+        print("turning on AC")  
+        globals()['cond'] = True
+    else:
+        print("Value not assingned and no action found")
+        globals()['cond'] =  False
+            
+    
 light()
